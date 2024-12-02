e15b892a97e1bdffa2dbcfbad54150c90812a657
Connor <connordavis115@gmail.com>
2024-12-02 03:08:01 +0000

    microdata added

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -21,21 +21,25 @@ export default function Projects() {
                         <a
                             href={project.link}
                             key={project.image}
-                            className="sm:w-1/2 w-full p-4">
+                            className="sm:w-1/2 w-full p-4"
+                            itemscope
+                            itemtype="https://schema.org/CreativeWork"
+                        >
                             <div className="flex relative h-64">
                                 <img
                                     alt="gallery of projects"
                                     className="absolute inset-0 w-full h-full object-cover object-center"
                                     src={project.image}
+                                    itemprop="image"
                                 />
                                 <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
-                                    <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
+                                    <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1" itemprop="genre">
                                         {project.subtitle}
                                     </h2>
-                                    <h1 className="title-font text-lg font-medium text-white mb-3">
+                                    <h1 className="title-font text-lg font-medium text-white mb-3" itemprop="name">
                                         {project.title}
                                     </h1>
-                                    <p className="leading-relaxed">{project.description}</p>
+                                    <p className="leading-relaxed" itemprop="description">{project.description}</p>
                                 </div>
                             </div>
                         </a>
